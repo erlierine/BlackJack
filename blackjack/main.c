32b0ee565529218025ec8532f05b9a178f92df3c
erlierine <ana97markovic@gmail.com>
2020-11-17 23:41:08 +0100

    feat(card): Implement struct
    Implement card struct and needed enum type.

--- blackjack/main.c
+++ blackjack/main.c
@@ -3,12 +3,34 @@
 #include <string.h>
 
 #define MSG_LENGTH 200
+#define MAX_PATH_LEN 100
+
+enum card_type{
+    C_ACE = 1,
+    C_2 = 2,
+    C_3 = 3,
+    C_4 = 4,
+    C_5 = 5,
+    C_6 = 6,
+    C_7 = 7,
+    C_8 = 8,
+    C_9 = 9,
+    C_10 = 10,
+    C_JACK = 12,
+    C_QUEEN = 13,
+    C_KING = 14
+};
 
 void error(char* msg, int exit_code)
 {
     printf ("%s\n", msg);
     exit(exit_code);
 }
+
+typedef struct Card{
+    enum card_type type;
+    char image_path[MAX_PATH_LEN];
+} card;
 
 int main(int argc, char** argv)
 {
